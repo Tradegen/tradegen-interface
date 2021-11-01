0b2652c99b095b7d2556502ec93a22d0d4d83350
Xavier Negron <xaviernegron888@gmail.com>
2021-10-31 23:28:15 -0500

    Update styling of Manage NFT Pool page

--- src/pages/Manage/ManageNFTPool.tsx
+++ src/pages/Manage/ManageNFTPool.tsx
@@ -9,21 +9,106 @@ import { ErrorBoundary } from '@sentry/react'
 import { formatNumber, formatPercent, formatBalance } from '../../functions/format'
 import Swap from '../Swap'
 
-const Buffer = styled.div`
+const TitleRow = styled.div`
   width: 100%;
+  color: white;
+  display: flex;
   background-color: none;
-  height: 15px;
+  margin-top: 30px;
 `
 
-const ButtonWrapper = styled.div`
-  width: 25%;
+const TitleRowContent = styled.div`
+  width: 24%;
+  text-align: left;
+`
+
+const FirstRow = styled.div`
+  width: 100%;
+  display: flex;
   background-color: none;
+  margin-top: 30px;
+  margin-bottom: 60px;
+`
+
+const FirstRowLeft = styled.div`
+  width: 30%;
+  color: white;
+  float: left;
+  background-color: none;
+  font-size: 30px;
+`
+
+const FactsheetTitle = styled.div`
+  width: 100%;
+  font-size: 22px;
+  color: white;
+`
+
+const FactsheetContent = styled.div`
+  width: 100%;
+  background-color: rgba(86,86,86,0.15);
+  border: 1px solid rgba(86,86,86,0.15);
+  border-radius: 8px;
+  padding-top: 5px;
+  margin-top: 30px;
+  padding-left: 20px;
+  margin-bottom: 30px;
+`
+
+const MiddleRow = styled.div`
+  width: 100%;
+  display: flex;
+  background-color: none;
+  margin-top: 30px;
+  margin-bottom: 60px;
+`
+
+const MiddleRowItem = styled.div`
+  width: 30%;
+  color: white;
+  background-color: rgba(86,86,86,0.15);
+  margin-left: 4%;
+  height: 60px;
+  border: 1px solid rgba(86,86,86,0.15);
+  border-radius: 8px;
+  text-align: center;
+  padding-top: 5px;
+`
+
+const MiddleRowItemTop = styled.div`
+  width: 100%;
+  display: block;
+  color: #C3C5CB;
+`
+
+const MiddleRowItemBottom = styled.div`
+  width: 100%;
+  display: block;
+  margin-top: 5px;
 `
 
 const ItemWrapper = styled.div`
   margin-top: 1rem;
   margin-bottom: 1rem;
+  min-width:1000px;
 `
+
+const SwapWrapper = styled.div`
+  width: 100%;
+  padding-left: 25%;
+  margin-top: 50px;
+`
+
+function getColour(totalReturn:string)
+{
+    if (totalReturn.charAt(0) == '-')
+    {
+        return 'rgba(248,113,113,1)'
+    }
+
+    return 'rgba(52,211,153,1)'
+}
+
 
 export default function ManageNFTPoolPage({
         match: {
@@ -42,37 +127,106 @@ export default function ManageNFTPoolPage({
 
     let manager = poolInfo.manager ?? ZERO_ADDRESS;
 
-    return (
-        <>
-            <p>Manage NFT Pool</p>
-            <p>Positions:</p>
-            <ItemWrapper>
-                {poolInfo.positionAddresses?.length === 0 ? (
-                    <div>No positions yet.</div>
-                ) : (
-                poolInfo.positionAddresses.map((address:string) => (
-                    <ErrorBoundary key={address}>
-                        <p>{address}</p>
-                    </ErrorBoundary>
-                )))}
-            </ItemWrapper>
-            <p>Balances:</p>
-            <ItemWrapper>
-                {poolInfo.positionBalances?.length === 0 ? (
-                    <div>No positions yet.</div>
-                ) : (
-                poolInfo.positionBalances.map((balance:bigint) => (
-                    <ErrorBoundary>
-                        <p>{formatBalance(balance)}</p>
-                    </ErrorBoundary>
-                )))}
-            </ItemWrapper>
+    let combinedPositions = [];
+    if (!poolInfo.positionBalances || !poolInfo.positionNames || !poolInfo.positionSymbols || poolInfo.positionBalances.length != poolInfo.positionNames.length)
+    {
+        combinedPositions = [];
+    }
+    else
+    {
+        for (var i = 0; i < poolInfo.positionNames.length; i++)
+        {
+            combinedPositions.push({
+                symbol: poolInfo.positionSymbols[i],
+                name: poolInfo.positionNames[i],
+                type: "Asset",
+                balance: poolInfo.positionBalances[i]
+            });
+        }
+    }
 
-            <Swap
-                poolAddress={id}
-                manager={manager}
-                isNFTPool={true}
-            ></Swap>
-        </>
+    return (
+        <ItemWrapper>
+            <FirstRow>
+                <FirstRowLeft>
+                    {poolInfo.name}
+                </FirstRowLeft>
+            </FirstRow>
+            <MiddleRow>
+                <MiddleRowItem style={{marginLeft: '0%'}}>
+                    <MiddleRowItemTop>
+                        TVL
+                    </MiddleRowItemTop>
+                    <MiddleRowItemBottom>
+                        {formatNumber(Number(poolInfo.TVL) / 100, true, true, 18)}
+                    </MiddleRowItemBottom>
+                </MiddleRowItem>
+                <MiddleRowItem>
+                    <MiddleRowItemTop>
+                        Token Price
+                    </MiddleRowItemTop>
+                    <MiddleRowItemBottom>
+                        {formatNumber(Number(poolInfo.tokenPrice) / 100, true, true, 18)}
+                    </MiddleRowItemBottom>
+                </MiddleRowItem>
+                <MiddleRowItem>
+                    <MiddleRowItemTop>
+                        Total Return
+                    </MiddleRowItemTop>
+                    <MiddleRowItemBottom style={{color:getColour(poolInfo.totalReturn)}}>
+                        {poolInfo.totalReturn}
+                    </MiddleRowItemBottom>
+                </MiddleRowItem>
+            </MiddleRow>
+            {combinedPositions.length > 0 && (
+                <>
+                    <FactsheetTitle>
+                        Positions
+                    </FactsheetTitle>
+                    <FactsheetContent style={{paddingTop: '20px', paddingBottom: '20px'}}>
+                        <TitleRow style={{marginTop: '0px'}}>
+                            <TitleRowContent>
+                                Symbol
+                            </TitleRowContent>
+                            <TitleRowContent>
+                                Name
+                            </TitleRowContent>
+                            <TitleRowContent>
+                                Type
+                            </TitleRowContent>
+                            <TitleRowContent>
+                                Balance
+                            </TitleRowContent>
+                        </TitleRow>
+                        {combinedPositions.map((element:any) => (
+                            <ErrorBoundary key={element.symbol}>
+                                <TitleRow>
+                                    <TitleRowContent>
+                                        {element.symbol}
+                                    </TitleRowContent>
+                                    <TitleRowContent>
+                                        {element.name}
+                                    </TitleRowContent>
+                                    <TitleRowContent>
+                                        {element.type}
+                                    </TitleRowContent>
+                                    <TitleRowContent>
+                                        {formatBalance(element.balance)}
+                                    </TitleRowContent>
+                                </TitleRow>
+                            </ErrorBoundary>
+                        ))}
+                    </FactsheetContent>
+                </>
+            )}
+
+            <SwapWrapper>
+                <Swap
+                    poolAddress={id}
+                    manager={manager}
+                    isNFTPool={true}
+                ></Swap>
+            </SwapWrapper>
+        </ItemWrapper>
     )
 }
