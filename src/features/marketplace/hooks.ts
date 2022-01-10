2bb75dc0fac804f6f901d09f23d2f7b798becc9e
Xavier Negron <xaviernegron888@gmail.com>
2022-01-09 22:48:53 -0800

    Write hooks for getting marketplace listing

--- src/features/marketplace/hooks.ts
+++ src/features/marketplace/hooks.ts
@@ -1,278 +1,57 @@
 import { Dispatch, useCallback, useEffect, useMemo, useState } from 'react'
-import { useNFTPoolContract, useTokenContract } from '../../hooks/useContract'
-import { useSingleCallResult, NEVER_RELOAD, useMultipleContractSingleData } from '../../state/multicall/hooks'
-import { useContractKit, useProvider } from '@celo-tools/use-contractkit'
-import { formatNumber, formatPercent, formatBalance } from '../../functions/format'
-import ERC20_INTERFACE from '../../constants/abis/erc20'
+import { useMarketplaceContract } from '../../hooks/useContract'
+import { useSingleCallResult } from '../../state/multicall/hooks'
 
-export interface NFTPoolInfo {
-  TVL: bigint | null,
-  address: string,
-  name: string | null,
-  tokenPrice: bigint | null,
-  totalReturn: string,
-  manager: string,
-  maxSupply: bigint | null,
-  seedPrice: bigint | null,
-  positionAddresses: string[],
-  positionBalances: bigint[],
-  positionNames: string[],
-  positionSymbols: string[],
-  tokenBalancesPerClass: bigint[]
+export interface MarketplaceListing {
+  asset: string | null,
+  seller: string | null,
+  numberOfTokens: bigint | null,
+  price: bigint | null,
+  tokenClass: bigint | null
 }
 
-export interface UserInvestmentInfo {
-    userBalances: bigint[],
-    userUSDBalance: bigint
-}
-
-export interface PositionsAndTotal {
-    positions: string[],
-    balances: bigint[],
-    total: bigint
-}
-
-export interface ManagerInfo {
-  manager: string
-}
-
-export function useTokenPrice(
-    NFTPoolContract: any,
+export function useListingIndex(
+    MarketplaceContract: any,
+    user: string,
+    asset: string
   ): bigint {
   
-    const tokenPrice = useSingleCallResult(NFTPoolContract, 'tokenPrice', undefined);
+    const index = useSingleCallResult(MarketplaceContract, 'getListingIndex', [user, asset]);
   
     return useMemo(() => {
-      return !tokenPrice || tokenPrice.loading
+      return !index || index.loading
+        ? BigInt(0)
+        : index?.result?.[0];
+    }, [index])
+}
+
+export function useMarketplaceListing(
+    MarketplaceContract: any,
+    listingIndex: bigint,
+  ): [string, string, bigint, bigint, bigint] {
+  
+    const marketplaceListing = useSingleCallResult(MarketplaceContract, 'getMarketplaceListing', [Number(listingIndex.toString())]);
+  
+    return useMemo(() => {
+      return !marketplaceListing || marketplaceListing.loading
         ? []
-        : tokenPrice?.result?.[0];
-    }, [tokenPrice])
+        : marketplaceListing?.result?.[0];
+    }, [marketplaceListing])
 }
 
-export function useAvailableTokensPerClass(
-    NFTPoolContract: any,
-  ): bigint[] {
+export function useMarketplaceListingInfo(marketplaceAddress:string, user:string, asset:string): MarketplaceListing {
+  const MarketplaceContract = useMarketplaceContract(marketplaceAddress);
+  
+  let index = useListingIndex(MarketplaceContract, user, asset);
+  index = index ?? BigInt(0);
 
-    const balances = useSingleCallResult(NFTPoolContract, 'getAvailableTokensPerClass', undefined);
-  
-    return useMemo(() => {
-      return !balances || balances.loading
-        ? [BigInt(0), BigInt(0), BigInt(0), BigInt(0)]
-        : [balances?.result?.[0], balances?.result?.[1], balances?.result?.[2], balances?.result?.[3]];
-    }, [balances])
-}
-
-export function useUserBalance(
-    NFTPoolContract: any,
-    user: string
-  ): bigint[] {
-  
-    const balances = useSingleCallResult(NFTPoolContract, 'getTokenBalancePerClass', [user]);
-  
-    return useMemo(() => {
-      return !balances || balances.loading
-        ? []
-        : [balances?.result?.[0], balances?.result?.[1], balances?.result?.[2], balances?.result?.[3]];
-    }, [balances])
-}
-
-export function useOwnerBalance(
-    NFTPoolContract: any,
-    user: string
-  ): bigint {
-  
-    const balance = useSingleCallResult(NFTPoolContract, 'balance', [user], NEVER_RELOAD);
-  
-    return useMemo(() => {
-      return !balance || balance.loading
-        ? []
-        : balance?.result?.[0];
-    }, [balance])
-}
-
-export function useName(
-    NFTPoolContract: any,
-  ): string {
-  
-    const name = useSingleCallResult(NFTPoolContract, 'name', undefined);
-  
-    return useMemo(() => {
-      return !name || name.loading
-        ? []
-        : name?.result?.[0];
-    }, [name])
-}
-
-export function useManager(
-    NFTPoolContract: any,
-  ): string {
-  
-    const manager = useSingleCallResult(NFTPoolContract, 'manager', undefined);
-  
-    return useMemo(() => {
-      return !manager || manager.loading
-        ? ""
-        : manager?.result?.[0];
-    }, [manager])
-}
-
-export function useUSDBalance(
-    NFTPoolContract: any,
-    user: string
-  ): bigint {
-
-    const balanceOf = useSingleCallResult(NFTPoolContract, 'getUSDBalance', [user]);
-  
-    return useMemo(() => {
-      return !balanceOf || balanceOf.loading
-        ? []
-        : balanceOf?.result?.[0];
-    }, [balanceOf])
-}
-
-export function useTotalSupply(
-    NFTPoolContract: any,
-  ): bigint {
-  
-    const totalSupply = useSingleCallResult(NFTPoolContract, 'totalSupply', undefined);
-  
-    return useMemo(() => {
-      return !totalSupply || totalSupply.loading
-        ? []
-        : totalSupply?.result?.[0];
-    }, [totalSupply])
-}
-
-export function useMaxSupply(
-    NFTPoolContract: any,
-  ): bigint {
-  
-    const maxSupply = useSingleCallResult(NFTPoolContract, 'maxSupply', undefined);
-  
-    return useMemo(() => {
-      return !maxSupply || maxSupply.loading
-        ? []
-        : maxSupply?.result?.[0];
-    }, [maxSupply])
-}
-
-export function useSeedPrice(
-    NFTPoolContract: any,
-  ): bigint {
-  
-    const price = useSingleCallResult(NFTPoolContract, 'seedPrice', undefined);
-  
-    return useMemo(() => {
-      return !price || price.loading
-        ? []
-        : price?.result?.[0];
-    }, [price])
-}
-
-export function usePositionsAndTotal(
-    NFTpoolContract: any,
-  ): any[] {
-  
-    const data = useSingleCallResult(NFTpoolContract, 'getPositionsAndTotal', undefined);
-  
-    return useMemo(() => {
-      return !data || data.loading
-        ? []
-        : [data?.result?.[0], data?.result?.[1], data?.result?.[2]];
-    }, [data])
-}
-
-export function useNFTPoolInfo(NFTPoolAddress:string): NFTPoolInfo {
-  const NFTPoolContract = useNFTPoolContract(NFTPoolAddress);
-  
-  const tokenPrice = useTokenPrice(NFTPoolContract);
-  const name = useName(NFTPoolContract);
-  const maxSupply = useMaxSupply(NFTPoolContract);
-  const seedPrice = useSeedPrice(NFTPoolContract);
-  const manager = useManager(NFTPoolContract);
-  const positionsAndTotal = usePositionsAndTotal(NFTPoolContract);
-  const tokenBalancesPerClass = useAvailableTokensPerClass(NFTPoolContract);
-
-  let positions = (!positionsAndTotal || positionsAndTotal[0] === undefined) ? [] : positionsAndTotal[0];
-  const symbols = usePositionSymbols(positions);
-  const names = usePositionNames(positions);
-
-  let totalReturn = "0%";
-  if (!tokenPrice || !seedPrice || BigInt(seedPrice) == BigInt(0))
-  {
-    totalReturn = "0%";
-  }
-  else
-  {
-    if (BigInt(tokenPrice) >= BigInt(seedPrice))
-    {
-      totalReturn = formatPercent(formatBalance((BigInt(tokenPrice) - BigInt(seedPrice)) * BigInt(100) * BigInt(1e18) / BigInt(seedPrice)));
-    }
-    else
-    {
-      totalReturn = "-" + formatPercent(formatBalance((BigInt(seedPrice) - BigInt(tokenPrice)) * BigInt(100) * BigInt(1e18) / BigInt(seedPrice)));
-    }
-  }
+  const marketplaceListing = useMarketplaceListing(MarketplaceContract, index);
 
   return {
-    TVL: (!positionsAndTotal || positionsAndTotal[2] === undefined) ? BigInt(0) : BigInt(positionsAndTotal[2]) / BigInt(1e16),
-    address: NFTPoolAddress,
-    name: name,
-    tokenPrice: (!tokenPrice) ? BigInt(0) : BigInt(tokenPrice) / BigInt(1e16),
-    totalReturn: totalReturn,
-    manager: manager,
-    maxSupply: (!maxSupply) ? BigInt(0) : BigInt(maxSupply),
-    seedPrice: (!seedPrice) ? BigInt(0) : BigInt(seedPrice) / BigInt(1e16),
-    positionAddresses: (!positionsAndTotal || positionsAndTotal[0] === undefined) ? [] : positionsAndTotal[0],
-    positionBalances: (!positionsAndTotal || positionsAndTotal[1] === undefined) ? [] : positionsAndTotal[1],
-    positionNames: names,
-    positionSymbols: symbols,
-    tokenBalancesPerClass: (!tokenBalancesPerClass || !tokenBalancesPerClass[0] || !tokenBalancesPerClass[1] || !tokenBalancesPerClass[2] || !tokenBalancesPerClass[3]) ? [BigInt(0), BigInt(0), BigInt(0), BigInt(0)] : tokenBalancesPerClass
-  }
-}
-
-export function useUserInvestmentInfo(NFTPoolAddress:string, userAddress:string): UserInvestmentInfo {
-    const NFTPoolContract = useNFTPoolContract(NFTPoolAddress);
-  
-    const userTokenBalance = useUserBalance(NFTPoolContract, userAddress);
-    const userUSDBalance = useUSDBalance(NFTPoolContract, userAddress);
-  
-    return {
-        userBalances: (!userTokenBalance) ? [BigInt(0), BigInt(0), BigInt(0), BigInt(0)] : userTokenBalance,
-        userUSDBalance: (!userUSDBalance) ? BigInt(0) : BigInt(userUSDBalance)
-    }
-}
-
-export function useTotalBalance(NFTPoolAddress:string, userAddress:string): bigint {
-  const NFTPoolContract = useNFTPoolContract(NFTPoolAddress);
-
-  const balance = useOwnerBalance(NFTPoolContract, userAddress);
-
-  return balance ?? BigInt(0)
-}
-
-export function usePositionSymbols(positions:string[]): string[] {
-  const names = useMultipleContractSingleData(positions, ERC20_INTERFACE, 'symbol')?.map((element:any) => (element?.result ? element?.result[0] : null));
-
-  return names ?? [];
-}
-
-export function usePositionNames(positions:string[]): string[] {
-  const names = useMultipleContractSingleData(positions, ERC20_INTERFACE, 'name')?.map((element:any) => (element?.result ? element?.result[0] : null));
-
-  return names ?? [];
-}
-
-export function useManagerInfo(poolAddress:string): ManagerInfo {
-  let { network, account } = useContractKit();
-  const { chainId } = network
-  console.log(account);
-
-  const poolContract = useNFTPoolContract(poolAddress);
-
-  const manager = useManager(poolContract);
-
-  return {
-    manager: manager ?? ""
+    asset: (!marketplaceListing || !marketplaceListing[0]) ? "" : marketplaceListing[0],
+    seller: (!marketplaceListing || !marketplaceListing[1]) ? "" : marketplaceListing[1],
+    numberOfTokens: (!marketplaceListing || !marketplaceListing[2]) ? BigInt(0) : marketplaceListing[2],
+    price: (!marketplaceListing || !marketplaceListing[3]) ? BigInt(0) : marketplaceListing[3],
+    tokenClass: (!marketplaceListing || !marketplaceListing[4]) ? BigInt(0) : marketplaceListing[4],
   }
 }
